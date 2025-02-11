8bcbea50ebd29482fa3c8d2ccff67a48f523b321
jordaoerick <erickjordao@outlook.com>
2025-02-10 23:37:21 -0300

    updating code, i will continue tomorrow

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //In this project, you will be building a number sorter.
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+    event.preventDefault();
+
+    const inputValues = [...document.getElementsByClassName("values-dropdown")];
+}
+
+sortButton.addEventListener("click", sortInputArray);
